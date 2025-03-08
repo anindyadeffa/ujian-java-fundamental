2a120a600f7a0724593e5869ed71d82d2ddf59f3
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2025-03-08 14:24:34 +0700

    first commit

---
+++ src/main/java/com/juaracoding/nabilaujian1jf/ujian1/App.java
@@ -0,0 +1,13 @@
+package com.juaracoding.nabilaujian1jf.ujian1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
